e786a0f5a4691b50ac4f9518930f8a665324e604
Niels Odgaard <niel2647@edu.easj.dk>
2017-06-13 19:46:24 +0200

    changed server port

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import * as express from 'express';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 enableProdMode();
 
